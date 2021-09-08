96504676742eaba57133a0053f22f05ec319bbf9
srinadh1304 <srinadh1304@gmail.com>
2021-09-08 17:41:57 +0530

    ADDED: Method to ensure email address starts with alphabets

--- src/com/bridgelabz/emailpatternmatching/EmailValidator.java
+++ src/com/bridgelabz/emailpatternmatching/EmailValidator.java
@@ -1,7 +1,28 @@
 package com.bridgelabz.emailpatternmatching;
 
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidator {
+
+	public static boolean validateEmail(String email) {
+		Pattern pattern = Pattern.compile("^[a-zA-Z]{3,}");
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to email validator program");
+		System.out.println("Enter an email id");
+		Scanner sc = new Scanner(System.in);
+		String email = sc.nextLine();
+
+		if(validateEmail(email))
+			System.out.println("It is a valid email id");
+		else 
+			System.out.println("It is an invalid email id");
+
+		sc.close();
 	}
 }
